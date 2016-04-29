9c09ebd9b5a8e993fc9c3df8fd47d33a4cbd08f1
Huynh Tu Vinh <giicms@gmail.com>
2016-04-29 11:28:38 +0700

    components

---
+++ components/widgets/Categories.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace giicms\category\components\widgets;
+
+use Yii;
+use yii\base\Component;
+use yii\web\Application;
+use yii\base\InvalidConfigException;
+use giicms\category\models\Category;
+
+class Categories extends \yii\base\Widget
+{
+
+    public function init()
+    {
+        
+    }
+
+    public function run()
+    {
+//        echo $this->render('themes/demo/widgets/test');
+        echo 'dzcvfz';
+    }
+
+}
